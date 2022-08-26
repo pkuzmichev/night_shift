5d8ae7490db59940eb1184ea356ce26bf1a45cbc
Peter Kuzmichev <petya.kuzmichev@gmail.com>
2022-08-26 13:05:15 +0300

    change color temperature

---
+++ main.py
@@ -0,0 +1,8 @@
+import color_temperature
+from PIL import Image
+
+if __name__ == '__main__':
+    color_temperature.convert_temp(Image.open('img/before/bliss.jpg'), 2500)
+    
+    
+# TODO: add arguments
