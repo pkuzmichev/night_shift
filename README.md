56efc3497a44ba32f9c79245a948b5c79675d5af
Peter Kuzmichev <petya.kuzmichev@gmail.com>
2022-08-28 10:44:24 +0300

    refactoring and write readme.md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-~
+## Change color temperature on photo
+
+Use diaposon from **1000** — **10000**
+
+![color-temperature-scale.jpg](color-temperature-scale.jpg)
+
+**Example:**
+`python main.py *path_to_file* *color_temp*`
+`python main.py img/before/bliss.jpg 2500`
+
+Before:
+![img/before/bliss.jpg](img/before/bliss.jpg)
+After:
+![img/after/bliss.jpg](img/after/bliss.jpg)
--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from PIL import Image
 import sys
 
 if __name__ == '__main__':
-    print(str(sys.argv[1]), sys.argv[2])
     color_temperature.convert_temp(Image.open(str(sys.argv[1])), int(sys.argv[2]))
-    # str(sys.argv[1]), sys.argv[2]
-    # color_temperature.convert_temp(Image.open('img/before/bliss.jpg'), 2500)
     
     
-# TODO: write readme.md
 # TODO: test (read rgb from image: min 3 tests)
